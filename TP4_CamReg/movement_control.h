f7eaf0130825f3a35052d51d27035848e039ccf1
MindTheGapM8 <nictermote2000@gmail.com>
2021-05-04 10:56:56 +0200

    Added promity sensor pointer init in movement controll

--- TP4_CamReg/movement_control.h
+++ TP4_CamReg/movement_control.h
@@ -2,7 +2,7 @@
 #define MOOVEMENT_CONTROL_H
 typedef unsigned char   uint8_t;
 
-void movement_init();
+void movement_init(proximity_msg_t* proximity;);
 uint8_t object_detection();
 void avoid_obstacle();
 void go_forward();
