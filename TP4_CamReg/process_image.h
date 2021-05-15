56656617f528ec1c8eb71688fb90a3eabe75f23a
LucasMutschler <48597126+LucasMutschler@users.noreply.github.com>
2021-05-15 19:13:08 +0200

    define deplacer
    define pour pi regulator et process image

--- TP4_CamReg/process_image.h
+++ TP4_CamReg/process_image.h
@@ -4,10 +4,20 @@
 float get_distance_cm(void);
 uint16_t get_line_position(void);
 void process_image_start(void);
-void set_target_reached(uint8_t value);
-uint8_t get_target_reached(void);
-uint8_t get_avoiding_obstacle(void);
-void set_avoiding_obstacle (uint8_t value);
-uint8_t get_line_not_found (void);
+
+//constants for the different parts of the pi regulator and process image
+
+#define IMAGE_BUFFER_SIZE		640
+#define WIDTH_SLOPE				5
+#define MIN_LINE_WIDTH			40
+#define ROTATION_THRESHOLD		10
+#define ROTATION_COEFF			2
+#define PXTOCM					600.0f //experimental value
+#define GOAL_DISTANCE 			5.0f
+#define MAX_DISTANCE 			25.0f
+#define ERROR_THRESHOLD			0.1f	//[cm] because of the noise of the camera
+#define KP						600.0f  //experimental value
+#define KI 						3.5f	//experimental value
+#define MAX_SUM_ERROR 			(MOTOR_SPEED_LIMIT/KI)
 
 #endif /* PROCESS_IMAGE_H */
