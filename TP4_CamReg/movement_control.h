b8869aefc7b868ee566396cb7a413fdb3b607a9a
Lucas <Lucas@DESKTOP-ONLANMK>
2021-05-15 19:25:04 +0200

    conflit reglé

--- TP4_CamReg/movement_control.h
+++ TP4_CamReg/movement_control.h
@@ -2,6 +2,14 @@
 #ifndef MOVEMENT_CONTROL
 #define MOVEMENT_CONTROL
 
+#ifndef MOVEMENT_CONTROL
+#define MOVEMENT_CONTROL
+
+
+#ifndef MOVEMENT_CONTROL
+#define MOVEMENT_CONTROL
+
+
 void pi_regulator_start(void);
 void movement_control_start(void);
 
