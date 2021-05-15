b912b347e8f9718507e89f9828e9d7576f22bed9
LucasMutschler <48597126+LucasMutschler@users.noreply.github.com>
2021-05-15 19:06:07 +0200

    semaphore and code clean
    chfprint enlever etc

--- TP4_CamReg/control_lights.c
+++ TP4_CamReg/control_lights.c
@@ -5,14 +5,14 @@
 
 #include <main.h>
 #include <leds.h>
+#include "audio/audio_thread.h"
+#include "audio/play_melody.h"
+#include "audio/play_sound_file.h"
 #include <control_lights.h>
 
-#define CHOREOGRAPHY_LENGTH     5000 //ms
-#define LED_INTENSITY           100
-#define LIGHT_FLICKER_TIME      300  //ms
 
-static BSEMAPHORE_DECL(goal_reached_sem, FALSE);
-static BSEMAPHORE_DECL(sirens_on_sem, TRUE);
+static BSEMAPHORE_DECL(goal_reached_sem, TRUE);			// semaphore to signal when the robot is correctly arrived
+static BSEMAPHORE_DECL(sirens_on_sem, FALSE);			// semaphore for light blinking when traveling to goal
 
 uint8_t goal_reached = FALSE;
 
@@ -21,30 +21,34 @@ void start_light_choreography(void){
     chBSemSignal(&goal_reached_sem);
 }
 
+/* Thread for light blinking when advancing */
+
 static THD_WORKING_AREA(waLightsSirens, 256);
 static THD_FUNCTION(LightsSirens, arg) {
 
     chRegSetThreadName(__FUNCTION__);
     (void)arg;
 
-    while(1){
+    while(1) {
 
         chBSemWait(&sirens_on_sem);
+
         set_led(LED1, LED_INTENSITY);
         set_led(LED3, LED_INTENSITY);
-        set_led(LED5, LED_INTENSITY);
         set_led(LED7, LED_INTENSITY);
-        
+
         toggle_rgb_led(LED2, BLUE_LED ,LED_INTENSITY);
 
-        while (goal_reached = FALSE)){
-
+        while (goal_reached == FALSE) {
             chThdSleepMilliseconds(LIGHT_FLICKER_TIME);
             toggle_rgb_led(LED2, BLUE_LED ,LED_INTENSITY);
             toggle_rgb_led(LED8, BLUE_LED ,LED_INTENSITY);
         }
+        clear_leds();
     }
 }
+
+/* Thread for light choreography when  motors are stopped (when goal is reached) */
 
 static THD_WORKING_AREA(waLightsCircling, 256);
 static THD_FUNCTION(LightsCircling, arg) {
@@ -52,40 +56,37 @@ static THD_FUNCTION(LightsCircling, arg) {
     chRegSetThreadName(__FUNCTION__);
     (void)arg;
 
-    systime_t time = chVTGetSystemTime();
+    systime_t time;
 
-    while(1){
+    while(1) {
 
-        chBSemWait(&goal_reached_sem)
+        chBSemWait(&goal_reached_sem);			// signal that the goal is reached
+        chThdSleepMilliseconds(20);
         clear_leds();
+        time = chVTGetSystemTime();
 
-        toggle_rgb_led(LED2, BLUE_LED ,LED_INTENSITY);
-        chThdSleepMilliseconds(LIGHT_FLICKER_TIME);
-        toggle_rgb_led(LED4, BLUE_LED ,LED_INTENSITY);
-
-        while (chVTGetSystemTime() < time + MS2ST(CHOREOGRAPHY_LENGTH)){
+        while (chVTGetSystemTime() < time + MS2ST(CHOREOGRAPHY_LENGTH)) {
 
             chThdSleepMilliseconds(LIGHT_FLICKER_TIME);
-            toggle_rgb_led(LED6, BLUE_LED ,LED_INTENSITY);
-            toggle_rgb_led(LED2, BLUE_LED ,LED_INTENSITY);
-            chThdSleepMilliseconds(LIGHT_FLICKER_TIME);
-            toggle_rgb_led(LED8, BLUE_LED ,LED_INTENSITY);
             toggle_rgb_led(LED4, BLUE_LED ,LED_INTENSITY);
             chThdSleepMilliseconds(LIGHT_FLICKER_TIME);
-            toggle_rgb_led(LED2, BLUE_LED ,LED_INTENSITY);
             toggle_rgb_led(LED6, BLUE_LED ,LED_INTENSITY);
             chThdSleepMilliseconds(LIGHT_FLICKER_TIME);
-            toggle_rgb_led(LED4, BLUE_LED ,LED_INTENSITY);
             toggle_rgb_led(LED8, BLUE_LED ,LED_INTENSITY);
+            chThdSleepMilliseconds(LIGHT_FLICKER_TIME);
+            toggle_rgb_led(LED2, BLUE_LED ,LED_INTENSITY);
         }
+
         clear_leds();
         goal_reached = FALSE;
         chBSemSignal(&sirens_on_sem);
     }
 }
 
-void lights_start(void){
-    spi_comm_start();
-	chThdCreateStatic(waLightsCircling, sizeof(waLightsCircling), LOWPRIO, LightsCircling, NULL);
-    chThdCreateStatic(waLightsSirens, sizeof(waLightsSirens), LOWPRIO, LightsSirens, NULL);
+/* Fonction initializing threads for lights control */
+
+void lights_start(void) {
+    clear_leds();
+    chThdCreateStatic(waLightsSirens, sizeof(waLightsSirens), HIGHPRIO, LightsSirens, NULL);
+	chThdCreateStatic(waLightsCircling, sizeof(waLightsCircling), HIGHPRIO, LightsCircling, NULL);
 }
